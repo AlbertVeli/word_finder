3191d0d3fbe6930b1b8bba5f69067c5421ca92f1
Albert Veli <albert.veli@gmail.com>
2023-08-26 11:22:16 +0200

    Add files via upload
    Initial commit

---
+++ word_finder.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import gzip
+from itertools import permutations
+import multiprocessing
+
+alphabet = list('abcdeéfghijklmnopqrstuvwxyzåäö')
+
+# Use one array per starting letter to speed up search a bit
+sys.stdout.write('Building wordlist...')
+wl = {}
+for c in alphabet:
+    wl[c] = []
+wordlist = 'svenska.gz'
+with gzip.open(wordlist, 'rt') as f:
+    for line in f:
+        first = line[0]
+        wl[first].append(line.rstrip())
+print('done')
+
+printed = []
+
+# Try one permutation
+def try_one(perm):
+    candidate = ''.join(perm)
+    first = candidate[0]
+    if candidate in wl[first]:
+        if not candidate in printed:
+            print(candidate)
+            printed.append(candidate)
+
+# Use multiprocessing to call try_one()
+# on all CPU-cores in parallell
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(f'Usage {sys.argv[0]} <letters> <number of letters in word>')
+    letters = list(sys.argv[1])
+    num = int(sys.argv[2])
+    try:
+        with multiprocessing.Pool() as pool:
+            pool.map(try_one, permutations(letters, num))
+    except:
+        pool.close()
+        pool.join()
